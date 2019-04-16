ff0d6219908f207e13543bb5c47c2e25195b32ff
Zach Bloomquist <github@chary.us>
2019-04-16 18:29:38 -0400

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends React.PureComponent {
     render() {
         return (
             <Card className="card-footer">
-                Got a feature request or an issue? Post it on the <a href="https://github.com/flotwig/chromium-downloads">GitHub repo</a>!<br/>
+                Got a feature request or an issue? Post it on the <a href="https://github.com/cypress-io/chromium-downloads">GitHub repo</a>!<br/>
                 The Chromium™ open source project is a registered trademark of Google LLC. All rights reserved.
             </Card>
         )
