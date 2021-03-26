693eb2ed61164919b768733694fc9537dd4426b3
Tim Griesser <tgriesser10@gmail.com>
2021-03-26 14:36:58 -0400

    WIP

--- backend/db.js
+++ backend/db.js
@@ -1,43 +1,47 @@
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize");
 
-let DATABASE_URL = process.env.DATABASE_URL
+let DATABASE_URL = process.env.DATABASE_URL;
 
-if (process.env.NODE_ENV === 'production') {
-  DATABASE_URL += '?ssl=true'
+if (process.env.NODE_ENV === "production") {
+  DATABASE_URL += "?ssl=true";
 }
 
-const sequelize = new Sequelize(DATABASE_URL)
+const sequelize = new Sequelize(DATABASE_URL);
 
 class Build extends Sequelize.Model {}
-Build.init({
-  version: Sequelize.STRING,
-  os: Sequelize.STRING,
-  channel: Sequelize.STRING,
-  timestamp: Sequelize.DATE,
-  baseRevision: Sequelize.STRING,
-  artifactsRevision: Sequelize.STRING,
-  downloads: Sequelize.JSONB
-}, {
-  sequelize,
-  modelName: 'builds',
-  timestamps: false,
-  indexes: [
-    {
-      unique: true,
-      fields: ['version', 'os', 'channel', 'timestamp']
-    }
-  ],
-  pool: {
-    max: 4,
-    min: 1
+Build.init(
+  {
+    version: Sequelize.STRING,
+    os: Sequelize.STRING,
+    channel: Sequelize.STRING,
+    timestamp: Sequelize.DATE,
+    baseRevision: Sequelize.STRING,
+    artifactsRevision: Sequelize.STRING,
+    downloads: Sequelize.JSONB,
+  },
+  {
+    sequelize,
+    modelName: "builds",
+    timestamps: false,
+    indexes: [
+      {
+        unique: true,
+        fields: ["version", "os", "channel", "timestamp"],
+      },
+    ],
+    pool: {
+      max: 4,
+      min: 1,
+    },
   }
-})
+);
 
 function initialize() {
-  return sequelize.sync()
+  console.log(sequelize);
+  return sequelize.sync();
 }
 
 module.exports = {
   initialize,
-  Build
-}
+  Build,
+};
--- backend/index.js
+++ backend/index.js
@@ -1,54 +1,55 @@
-const db = require('./db')
-const express = require('express')
-const scraper = require('./scraper')
+const db = require("./db");
+const express = require("express");
+const scraper = require("./scraper");
 
-const PORT = Number(process.env.PORT) || 3001
+const PORT = Number(process.env.PORT) || 3001;
 
-const app = express()
+const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('access-control-allow-origin', '*')
+  res.setHeader("access-control-allow-origin", "*");
 
-  next()
-})
+  next();
+});
 
-app.get('/builds', (req, res) => {
+app.get("/builds", (req, res) => {
   db.Build.findAll({
-    attributes: ['version', 'os', 'channel', 'timestamp'],
-    order: [['timestamp', 'DESC']]
-  })
-  .then(builds => {
-    res.json(builds)
-  })
-})
+    attributes: ["version", "os", "channel", "timestamp"],
+    order: [["timestamp", "DESC"]],
+  }).then((builds) => {
+    res.json(builds);
+  });
+});
 
-app.get('/builds/:version/:channel/:os', (req, res) => {
+app.get("/builds/:version/:channel/:os", (req, res) => {
   db.Build.findAll({
     where: {
       channel: req.params.channel,
       os: req.params.os,
-      version: req.params.version
-    }
-  })
-  .then(builds => {
+      version: req.params.version,
+    },
+  }).then((builds) => {
     if (!builds.length) {
-      return res.sendStatus(404)
+      return res.sendStatus(404);
     }
 
-    res.json(builds[0])
-  })
-})
+    res.json(builds[0]);
+  });
+});
 
-console.log('Initializing')
+console.log("Initializing");
+
+console.log(db.initialize);
 
 db.initialize()
-.then(() => {
-  console.log('Starting scraping')
-  scraper.start()
+  .then(() => {
+    console.log("Starting scraping");
+    scraper.start();
 
-  app.listen(PORT, () => {
-    console.log(`Backend listening on ${PORT}.`)
+    app.listen(PORT, () => {
+      console.log(`Backend listening on ${PORT}.`);
+    });
   })
-}).catch(e => {
-  console.error(e)
-})
+  .catch((e) => {
+    console.error(e);
+  });
